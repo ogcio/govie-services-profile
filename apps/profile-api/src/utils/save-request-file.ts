111785690719d4745d93df4edafe939defc7d630
Matteo La Cognata <matteo.lacognata@gmail.com>
2025-01-20 16:34:40 +0100

    chore(): fix

--- apps/profile-api/src/utils/save-request-file.ts
+++ apps/profile-api/src/utils/save-request-file.ts
@@ -1,10 +1,11 @@
 import { httpErrors } from "@fastify/sensible";
 import type { FastifyRequest } from "fastify";
+import type { ImportProfilesBody } from "~/schemas/profiles/import-profiles.js";
 
 export const saveRequestFile = async (
   request: FastifyRequest,
 ): Promise<string> => {
-  const file = await request.files();
+  const file = await (request.body as ImportProfilesBody).file?.file;
   if (!file) {
     throw httpErrors.badRequest("File is missing in the request");
   }
