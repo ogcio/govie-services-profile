d56f784a63cc6da0ebbe8d3aba4f9d59ad2bb438
Samuele Salvatico <samuele.salvatico@nearform.com>
2025-01-21 14:14:09 +0100

    test: added test for webhook with direct signin

--- apps/profile-api/src/test/routes/webhooks/user-login-wh.test.ts
+++ apps/profile-api/src/test/routes/webhooks/user-login-wh.test.ts
@@ -66,6 +66,78 @@ describe("/user-login-wh", {}, () => {
     },
   };
 
+  const directSigninBody = {
+    ip: "100.64.0.214",
+    data: {
+      id: "c2uyjunodb03",
+      name: "Tony Stark",
+      avatar: null,
+      profile: {},
+      username: null,
+      createdAt: 1731395910235,
+      updatedAt: 1737461333587,
+      customData: {},
+      identities: {
+        "MyGovId (MyGovId connector)": {
+          userId: "7ffe40ff7d558de01c54",
+          details: {
+            id: "7ffe40ff7d558de01c54",
+            name: "Tony Stark",
+            email: "tony.stark@gov.ie",
+            phone: "+3535256052082",
+            rawData: {
+              aud: "mock_client_id",
+              exp: 1731403109,
+              iat: 1731395909,
+              iss: "https://mygovid-mock.dev.blocks.gov.ie",
+              nbf: 1716804749,
+              oid: "71848ec91433bc4222d0",
+              sub: "7ffe40ff7d558de01c54",
+              ver: "1.0",
+              email: "tony.stark@gov.ie",
+              mobile: "+3535256052082",
+              surname: "Stark",
+              lastName: "Stark",
+              BirthDate: "13/06/1941",
+              auth_time: 1731395909171,
+              firstName: "Tony",
+              givenName: "Tony",
+              CustomerId: "532",
+              LastJourney: "Login",
+              AlternateIds: "",
+              CorrelationId: "38c7bbd78acbb8e57fa28ded7147e017ebd3e18a",
+              SMS2FAEnabled: false,
+              DSPOnlineLevel: "0",
+              currentCulture: "en",
+              PublicServiceNumber: "0111019P",
+              AcceptedPrivacyTerms: true,
+              DSPOnlineLevelStatic: "0",
+              trustFrameworkPolicy: "B2C_1A_MyGovID_signin-v5-PARTIAL2",
+              AcceptedPrivacyTermsDateTime: 1715582120,
+              AcceptedPrivacyTermsVersionNumber: "7",
+            },
+          },
+        },
+      },
+      isSuspended: false,
+      lastSignInAt: 1737461333586,
+      primaryEmail: "tony.stark@gov.ie",
+      primaryPhone: "+3535256052082",
+      applicationId: "0921d8onfb9f3bv75trgf",
+    },
+    event: "User.Data.Updated",
+    hookId: "login-webhook",
+    createdAt: "2025-01-21T12:08:53.599Z",
+    sessionId: "B8J8FuBsBXI6ZLoHhfkyi",
+    userAgent:
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36 Edg/131.0.0.0",
+    application: {
+      id: "1lvmteh2ao3xrswyq7j3e",
+      name: "Messaging Building Block",
+      type: "Traditional",
+      description: "Messaging App of Life Events",
+    },
+  };
   const generateSignature = (payload: Buffer, secret: string) => {
     const hmac = createHmac("sha256", secret);
     hmac.update(payload);
@@ -161,5 +233,28 @@ describe("/user-login-wh", {}, () => {
 
     expect(response.statusCode).toBe(422);
     expect(processUserWebhook).not.toHaveBeenCalled();
+  });
+
+  it("should accept direct-signin webhook body", async () => {
+    // This body is the one that logto is sending
+    // when the user sign in directly without being
+    // created by the profile api "import invokation"
+    const payload = Buffer.from(JSON.stringify(directSigninBody));
+    const signature = generateSignature(
+      payload,
+      app.config.LOGTO_WEBHOOK_SIGNING_KEY,
+    );
+
+    const response = await app.inject({
+      method: "POST",
+      url: "/user-login-wh",
+      headers: {
+        "content-type": "application/json",
+        "logto-signature-sha-256": signature,
+      },
+      payload: directSigninBody,
+    });
+
+    expect(response.statusCode).toBe(200);
   });
 });
